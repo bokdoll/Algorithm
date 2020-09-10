5072c97a7b2742518bd066764dd332a93f7dd34d
bokdoll <winnerhj1109@naver.com>
2020-09-10 15:01:41 +0900

    [스택/큐] 프린터

--- Algorithm-Programmers/level-2/printer.py
+++ Algorithm-Programmers/level-2/printer.py
@@ -1,39 +1,72 @@
-def solution(priorities, location):
-    num = priorities[location]
-    nums = {}
+# 2020/09/10 (목)
+from collections import deque
 
-    # upperCnt = num보다 큰 수의 개수
-    # lastV = num보다 큰 수 중 가장 작은 수
+
+def solution(priorities, location):  # 큐 사용
+    num = priorities[location]
+    priorities[location] = -1
+    p = deque(priorities)
+    answer = 1
+
+    while True:
+        maxP = max(p)
+        popped = p.popleft()
+        if len(p) == 0:
+            return answer
+        else:
+            if maxP < num:
+                if popped == -1:
+                    return answer
+                else:
+                    p.append(popped)
+            elif maxP == num:
+                if popped == -1:
+                    return answer
+                else:
+                    if popped == maxP:
+                        answer += 1
+                    else:
+                        p.append(popped)
+            else:
+                if popped < maxP:
+                    p.append(popped)
+                else:
+                    answer += 1
+
+
+def solution2(priorities, location):    # 실패한 풀이
+    num = priorities[location]
+    nums = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    # upperCnt = num 보다 큰 수의 개수
+    # lastV = num 보다 큰 수 중 가장 작은 수
     upperCnt, lastV = 0, 0
 
     for i in range(1, 10):
-        nums[i] = priorities.count(i)
+        nums[i-1] = priorities.count(i)
 
     for i in range(9, num-1, -1):
-        if i > num and nums[i] > 0:
+        if i > num and nums[i-1] > 0:
             lastV = i
-    print(lastV)
 
     if lastV == 0:
         return priorities[:location+1].count(num)
 
     for i in range(num, 9):
-        upperCnt += list(nums.items())[i][1]
-    print(upperCnt)
+        upperCnt += nums[i]
 
     if priorities.count(num) == 1:
         return upperCnt+1
     else:
         copy = list(reversed(priorities))
         idx = len(priorities) - copy.index(lastV) - 1
-        print(idx)
         if location < idx:
-            return upperCnt + priorities[:location+1].count(num)+priorities[idx:].count(num)
+            return upperCnt + priorities[:location+1].count(num)+priorities[idx+1:].count(num)
         else:
-            return upperCnt + priorities[idx: location+1].count(num)
+            return upperCnt + priorities[idx+1: location+1].count(num)
 
 
 if __name__ == '__main__':
-    priorities = [1, 2, 3, 2]
-    location = 3
+    priorities = [2, 1, 3, 2]
+    location = 2
     print(solution(priorities, location))
