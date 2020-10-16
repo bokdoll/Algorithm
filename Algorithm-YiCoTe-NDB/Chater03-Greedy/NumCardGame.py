57efce364dc9702f651e0e7e1edb6619d77524ee
Hyeonju Lee <winnerhj1109@naver.com>
2020-10-16 16:24:06 +0900

    [Chapter03] 숫자카드게임

---
+++ Algorithm-YiCoTe-NDB/Chater03-Greedy/NumCardGame.py
@@ -0,0 +1,18 @@
+# 2020/10/16 (금)
+# [Chapter03-Greedy] 숫자 카드 게임
+
+import sys
+
+def solution():
+    lst = list(map(int, sys.stdin.readline().split()))
+    N = lst[0]
+    answer = []
+    for i in range(N):
+        lst = list(map(int, sys.stdin.readline().split()))
+        answer.append(min(lst))
+    return max(answer)
+
+
+
+if __name__ == "__main__":
+    print(solution())
